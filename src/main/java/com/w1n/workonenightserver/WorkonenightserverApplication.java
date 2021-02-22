d9624adf30fb49928483b10726c82c3fc1dc1ef6
Stemicio9 <s.miceli90@gmail.com>
2021-02-22 16:04:27 +0100

    Commit iniziale progetto finale

---
+++ src/main/java/com/w1n/workonenightserver/WorkonenightserverApplication.java
@@ -0,0 +1,13 @@
+package com.w1n.workonenightserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkonenightserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkonenightserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/w1n/workonenightserver/WorkonenightserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w1n.workonenightserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkonenightserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
